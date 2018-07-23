68dc056b5429e1e00f4f559e263eb31974724838
bogdan2020 <41567094+bogdan2020@users.noreply.github.com>
2018-07-23 10:40:39 +0300

    da

--- Employee.cs
+++ Employee.cs
@@ -17,7 +17,7 @@ namespace AccesaEmployee
 
         public string Name => _name;
         public EmployeePosition Position => _position;
-        public float Capacity => _capacity;
+        public float Capacity => _capacity; ygy
         public List<string> Hobbies => _hobbies;
 
         protected Employee(string name, EmployeePosition position, float capacity)
